756f60f8a018fe3676bfe039b9530b00d6d2d429
Anita <yachi77128230@gmail.com>
2023-09-24 02:13:53 +0800

    fizzbuzz available

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,11 @@
-## write your fizzbuzz method in this file
-# see http://en.wikipedia.org/wiki/Fizz_buzz for details on FizzBuzz game
+# frozen_string_literal: true
+
+def fizzbuzz(size, &block)
+  (1..size).map do |number|
+    result = ''
+    result += 'Fizz' if (number % 3).zero?
+    result += 'Buzz' if (number % 5).zero?
+    decorated_item = result.empty? ? number.to_s : result
+    block_given? ? block.call(decorated_item) : decorated_item
+  end
+end
